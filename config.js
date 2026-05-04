ac7a66be79ae16290be71d3f37ebcf16519c8180
a7896464-dev <a7896464@gmail.com>
2026-05-04 20:23:00 +0900

    Add public landing config

---
+++ config.js
@@ -0,0 +1,3 @@
+// Google Apps Script Web App URL.
+// Empty value means local/file test mode.
+window.FIRE_SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbwOwu_se5LenAkTnNL59nB-jCY_MamhtImttrScYrzy29c1mJsRXI6Ls0PBVeXp8WHI/exec";
